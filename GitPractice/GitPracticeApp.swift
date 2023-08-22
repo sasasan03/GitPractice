7f991a70c30c35c450b9d117153081046df1f7cb
sasasan03 <sako.hiro62@gmail.com>
2023-08-22 22:06:00 +0900

    init

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by sako0602 on 2023/08/22.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
