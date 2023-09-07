cf11568f309be70d89bcc5ba523b61fc30a63476
masaharu <dabao456democebu3@gmail.com>
2023-09-07 23:21:09 +0900

    Revert "とおれ！"
    This reverts commit b97af2b8dbbc76ac2ba6e9cd33230365202af98b.

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -14,7 +14,6 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("みんなの好きなアニメはなんですか？")
-            Text("とおれ！！プルリク！")
         }
         .padding()
         
